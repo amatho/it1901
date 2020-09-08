54333dc2238aedd706439676283b172a0585d30a
Erlend Vaboen <erlend@dhcp-10-22-4-85.wlan.ntnu.no>
2020-09-08 13:24:19 +0200

    .

--- src/main/java/core/Course.java
+++ src/main/java/core/Course.java
@@ -15,18 +15,12 @@ public class Course {
     public int getCourseLength() {
         return holes.size();
     }
+
     public List<Hole> getHoles() {
         return holes;
     }
 
-    public void checkValidHole(int index) {
-        if (index >= this.getCourseLength() || index < 0) {
-            throw new IllegalArgumentException("Hole does not exist");
-        }
-    }
-
     public Hole getHole(int index) {
-        this.checkValidHole(index);
         return holes.get(index);
     }
 }
