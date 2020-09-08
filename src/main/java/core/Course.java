1e30a6f15258cc9b53d6162a2fe478a26b06e426
Erlend Vaboen <erlend@dhcp-10-22-4-85.wlan.ntnu.no>
2020-09-08 13:15:14 +0200

    created Course and Hole

--- src/main/java/core/Course.java
+++ src/main/java/core/Course.java
@@ -1,9 +1,32 @@
 package core;
 
 import java.util.ArrayList;
-import java.util.Collection;
+import java.util.Arrays;
+import java.util.List;
 
 public class Course {
-    Collection<Hole> holes = new ArrayList<Hole>();
 
+    List<Hole> holes;
+
+    public Course(Hole... holes) {
+        this.holes = Arrays.asList(holes);
+    }
+
+    public int getCourseLength() {
+        return holes.size();
+    }
+    public List<Hole> getHoles() {
+        return holes;
+    }
+
+    public void checkValidHole(int index) {
+        if (index >= this.getCourseLength() || index < 0) {
+            throw new IllegalArgumentException("Hole does not exist");
+        }
+    }
+
+    public Hole getHole(int index) {
+        this.checkValidHole(index);
+        return holes.get(index);
+    }
 }
