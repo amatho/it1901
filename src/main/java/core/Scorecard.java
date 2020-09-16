1e41277fe92ff1c021dddee303d41bbb8ebb896a
Peder Stenberg-Nilsen <peder-sn@hotmail.com>
2020-09-16 12:27:49 +0200

    Fixed some minor errors

--- src/main/java/core/Scorecard.java
+++ src/main/java/core/Scorecard.java
@@ -1,32 +1,23 @@
 package core;
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
 
 public class Scorecard {
 
-    private Course course;
-   // private List<User> user;
-    //private List<List<Integer>> scorecard;
-    private HashMap<User, Array[]> scorecard;
+  private Course course;
+  private HashMap<User, int[]> scorecard;
 
 
-    public Scorecard(Course course, User ... users){
-        if(users.length > 4){
-            throw new IllegalArgumentException("Cannot have more than four users.");
-        }
-        this.course = course;
-        Array[] score = {null};
-        Arrays.fill(score, course.getCourseLength());
-        for(User u : users){
-            scorecard.put(u, score);
-        }
+  public Scorecard(Course course, User... users) {
+    if (users.length > 4) {
+      throw new IllegalArgumentException("Cannot have more than four users.");
     }
-
-
+    this.course = course;
+    int[] score = new int[course.getCourseLength()];
+    for (User u : users) {
+      scorecard.put(u, score);
+    }
+  }
 
 
 }
