c466f3469574726bca5ef6379e49bd2a8e608b4b
Erlend Vaboen <erlevab@stud.ntnu.no>
2020-10-06 14:56:00 +0200

    Implement GUI and controller for User

---
+++ golfapp-gui/src/main/java/golfapp/gui/UserController.java
@@ -0,0 +1,76 @@
+package golfapp.gui;
+
+import golfapp.core.Scorecard;
+import golfapp.core.User;
+import java.io.IOException;
+import java.util.Collection;
+import javafx.beans.property.ReadOnlyStringWrapper;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.stage.Stage;
+
+public class UserController {
+
+
+  private final User user = new User("Amandus");
+  @FXML
+  Label username;
+  @FXML
+  Button viewSelectedScorecard;
+  @FXML
+  Button logOut;
+  @FXML
+  TableView<Scorecard> scorecardTableView;
+  @FXML
+  TableColumn<Scorecard, String> courseColumn;
+  @FXML
+  TableColumn<Scorecard, String> timeColumn;
+
+  @FXML
+  void initialize() {
+    username.setText("Username: " + user.getUsername());
+    courseColumn.setCellValueFactory(
+        sc -> new ReadOnlyStringWrapper(sc.getValue().getCourse().getCourseName()));
+    timeColumn
+        .setCellValueFactory(sc -> new ReadOnlyStringWrapper(sc.getValue().getDate().toString()));
+    updateView();
+  }
+
+  private void updateView() {
+    Collection<Scorecard> tmp = user.getScoreCardHistory();
+    scorecardTableView.getItems().removeAll();
+    if (!tmp.isEmpty()) {
+      for (Scorecard sc : tmp) {
+        scorecardTableView.getItems().add(sc);
+      }
+    }
+  }
+
+  @FXML
+  void handleLogOutButton(ActionEvent event) throws IOException {
+    Parent courseParent = FXMLLoader
+        .load(getClass().getResource("LogIn.fxml")); //TODO: Implement LogIn.fxml
+    Scene courseScene = new Scene(courseParent);
+    Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+    window.setScene(courseScene);
+    window.show();
+  }
+
+  @FXML
+  void handleViewSelectedScorecardButton(ActionEvent event) throws IOException {
+    Parent courseParent = FXMLLoader
+        .load(getClass().getResource("ScoreCardView.fxml")); //TODO: Implement ScoreCardView.fxml
+    Scene courseScene = new Scene(courseParent);
+    Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+    window.setScene(courseScene);
+    window.show();
+  }
+}
