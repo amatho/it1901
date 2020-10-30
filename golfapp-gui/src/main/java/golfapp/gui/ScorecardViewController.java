860579aea3f0b03c201ac0135dda9ddb53452b71
Peder Stenberg-Nilsen <peder-sn@hotmail.com>
2020-10-30 12:52:16 +0100

    Finish GUI for scorecardView

--- golfapp-gui/src/main/java/golfapp/gui/ScorecardViewController.java
+++ golfapp-gui/src/main/java/golfapp/gui/ScorecardViewController.java
@@ -22,7 +22,8 @@ public class ScorecardViewController {
   private final Scorecard scorecard;
   private final Course course;
   private final List<Hole> holes;
-  private double prefWidth;
+  private double vboxPrefWidth = 0;
+  private final double widthLeftLabel = 120;
 
   @FXML
   VBox leftInfo;
@@ -32,75 +33,133 @@ public class ScorecardViewController {
   HBox holeLength;
   @FXML
   HBox holePar;
+  @FXML
+  Label infoLabel;
 
-
+  /**
+   * Create a controller for ScorecardViewController.
+   *
+   * @param appManager takes in appManager
+   * @param scorecard takes in the relevant scorecard
+   */
   public ScorecardViewController(AppManager appManager, Scorecard scorecard) {
     this.appManager = appManager;
     this.scorecard = scorecard;
     this.course = scorecard.getCourse();
     this.holes = course.getHoles();
-    prefWidth = course.getCourseLength() * 10;
   }
 
   @FXML
   void initialize() {
-    double height = hBoxSetUp();
+    double height = horizontalBoxSetup();
     leftInfo.setBorder(new Border(new BorderStroke(Color.BLACK,
         BorderStrokeStyle.SOLID, CornerRadii.EMPTY, new BorderWidths(2))));
     leftInfo.setPrefHeight(height);
-    //leftInfo.setPrefWidth();
+    leftInfo.setPrefWidth(vboxPrefWidth);
+    infoLabel.setText("Name: " + course.getName()
+        + "         Date: " + scorecard.getDate());
+  }
+
+  @FXML
+  void handleGoToUser() {
+    appManager.loadView("User.fxml", UserController::new);
   }
 
   private double playerSetup() {
     double heightPlayers = 0.0;
     Set<String> users = scorecard.getUsers();
     for (String s : users) {
-      HBox h = new HBox();
-      h.getChildren().add(new Label(s));
-      leftInfo.getChildren().add(h);
-      h.setSpacing(10);
-      h.setAlignment(Pos.CENTER_LEFT);
-      h.setPrefHeight(80);
-      h.setBorder(new Border(new BorderStroke(Color.BLACK,
+      Label label = new Label(s);
+      label.setBorder(new Border(new BorderStroke(Color.BLACK,
           BorderStrokeStyle.SOLID, CornerRadii.EMPTY, new BorderWidths(1))));
-      heightPlayers = heightPlayers +  80;
+      label.setPrefWidth(widthLeftLabel);
+      label.setAlignment(Pos.CENTER);
+      HBox h = new HBox();
+      h.getChildren().add(label);
+
+      //method for now invalid
+      /*for (Hole hole : holes) {
+        int score = scorecard.getScore(s, hole); // invalid type for now.
+        Label scoreEachHole = new Label(Integer.toString(score));
+        scoreEachHole.setBorder(new Border(new BorderStroke(Color.BLACK,
+            BorderStrokeStyle.SOLID, CornerRadii.EMPTY, new BorderWidths(1))));
+        scoreEachHole.setPrefWidth(widthLeftLabel);
+        scoreEachHole.setAlignment(Pos.CENTER);
+        h.getChildren().add(scoreEachHole);
+      }*/
+
+      leftInfo.getChildren().add(h);
+      h.setPrefHeight(80);
+      heightPlayers = heightPlayers +  h.getHeight();
       h.setStyle("-fx-font: 24 arial;");
     }
+
     holePar = new HBox();
-    holePar.setPrefHeight(80);
+    holePar.setPrefHeight(60);
+    heightPlayers += holePar.getHeight();
     leftInfo.getChildren().add(holePar);
     return heightPlayers;
   }
 
-  private double hBoxSetUp() {
+  private double horizontalBoxSetup() {
     int i = 1;
-    holeLength.getChildren().add(new Label("Lengde:"));
-    holeIndex.getChildren().add(new Label("Hull:"));
+    Label holeLengthLabel = new Label("Length:");
+    holeLengthLabel.setPrefWidth(widthLeftLabel);
+    holeLengthLabel.setBorder(new Border(new BorderStroke(Color.BLACK,
+        BorderStrokeStyle.SOLID, CornerRadii.EMPTY, new BorderWidths(1))));
+    holeLengthLabel.setAlignment(Pos.CENTER);
+    holeLength.getChildren().add(holeLengthLabel);
+
+    Label holeIndexLabel = new Label("Hole:");
+    holeIndexLabel.setPrefWidth(widthLeftLabel);
+    holeIndexLabel.setBorder(new Border(new BorderStroke(Color.BLACK,
+        BorderStrokeStyle.SOLID, CornerRadii.EMPTY, new BorderWidths(1))));
+    holeIndexLabel.setAlignment(Pos.CENTER);
+    holeIndex.getChildren().add(holeIndexLabel);
+
+    vboxPrefWidth += holeLengthLabel.getPrefWidth();
     double heightVbox = playerSetup();
-    heightVbox += 3 * 80.0;
-    holePar.getChildren().add(new Label("Par:"));
+    heightVbox += holeIndex.getHeight() + holeLength.getHeight();
+
+    Label holeParLabel = new Label("Par:");
+    holeParLabel.setPrefWidth(widthLeftLabel);
+    holeParLabel.setBorder(new Border(new BorderStroke(Color.BLACK,
+        BorderStrokeStyle.SOLID, CornerRadii.EMPTY, new BorderWidths(1))));
+    holeParLabel.setAlignment(Pos.CENTER);
+    holePar.getChildren().add(holeParLabel);
 
     for (Hole h : holes) {
-      holeLength.getChildren().add(new Label(Double.toString(h.getLength())));
-      holePar.getChildren().add(new Label(Integer.toString(h.getPar())));
-      holeIndex.getChildren().add(new Label(Integer.toString(i)));
+      Label label1 = new Label(Double.toString(h.getLength()));
+      Label label2 = new Label(Integer.toString(h.getPar()));
+      Label label3 = new Label(Integer.toString(i));
+
+      label1.setBorder(new Border(new BorderStroke(Color.BLACK,
+          BorderStrokeStyle.SOLID, CornerRadii.EMPTY, new BorderWidths(1))));
+      label2.setBorder(new Border(new BorderStroke(Color.BLACK,
+          BorderStrokeStyle.SOLID, CornerRadii.EMPTY, new BorderWidths(1))));
+      label3.setBorder(new Border(new BorderStroke(Color.BLACK,
+          BorderStrokeStyle.SOLID, CornerRadii.EMPTY, new BorderWidths(1))));
+
+      double widthOtherLabels = 80;
+      label1.setPrefWidth(widthOtherLabels);
+      label2.setPrefWidth(widthOtherLabels);
+      label3.setPrefWidth(widthOtherLabels);
+
+      label1.setAlignment(Pos.CENTER);
+      label2.setAlignment(Pos.CENTER);
+      label3.setAlignment(Pos.CENTER);
+
+      holeLength.getChildren().add(label1);
+      holePar.getChildren().add(label2);
+      holeIndex.getChildren().add(label3);
+
+      vboxPrefWidth += label1.getPrefWidth() + 2; // +borderWidth
       i++;
     }
-    holeIndex.setSpacing(10);
-    holeLength.setSpacing(10);
-    holePar.setSpacing(10);
+
     holeIndex.setStyle("-fx-font: 24 arial; -fx-font-weight: bold");
     holeLength.setStyle("-fx-font: 24 arial; -fx-font-weight: bold");
     holePar.setStyle("-fx-font: 24 arial; -fx-font-weight: bold");
-    holeIndex.setAlignment(Pos.CENTER_LEFT);
-    holeLength.setAlignment(Pos.CENTER_LEFT);
-    holePar.setAlignment(Pos.CENTER_LEFT);
-    holeIndex.setBorder(new Border(new BorderStroke(Color.BLACK,
-        BorderStrokeStyle.SOLID, CornerRadii.EMPTY, new BorderWidths(1))));
-    holeLength.setBorder(new Border(new BorderStroke(Color.BLACK,
-        BorderStrokeStyle.SOLID, CornerRadii.EMPTY, new BorderWidths(1))));
-    holePar.setBorder(new Border(new BorderStroke(Color.BLACK,
-        BorderStrokeStyle.SOLID, CornerRadii.EMPTY, new BorderWidths(1))));
 
     return heightVbox;
   }
