cc2b83c385982cfd666817807d1981df1c56ec5f
Amandus Søve Thorsrud <ama.thorsrud@gmail.com>
2020-11-07 11:47:57 +0100

    Add AbstractControllerTest for easier GUI testing
    Closes #57

--- golfapp-gui/src/test/java/golfapp/gui/LogInControllerTest.java
+++ golfapp-gui/src/test/java/golfapp/gui/LogInControllerTest.java
@@ -1,16 +1,10 @@
 package golfapp.gui;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-
 import java.io.IOException;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.testfx.api.FxRobot;
@@ -18,36 +12,21 @@ import org.testfx.framework.junit5.ApplicationExtension;
 import org.testfx.framework.junit5.Start;
 
 @ExtendWith(ApplicationExtension.class)
-public class LogInControllerTest {
-
-  private LogInController controller;
-
-  @BeforeAll
-  static void headless() {
-    if (Boolean.getBoolean("gitlab-ci")) {
-      System.setProperty("java.awt.headless", "true");
-      System.setProperty("testfx.robot", "glass");
-      System.setProperty("testfx.headless", "true");
-      System.setProperty("prism.order", "sw");
-      System.setProperty("prism.text", "t2k");
-    }
-  }
+public class LogInControllerTest extends AbstractControllerTest<LogInController> {
 
   @Start
   void start(final Stage stage) throws IOException {
-    final var loader = new FXMLLoader(getClass().getResource("LogIn.fxml"));
-    loader.setControllerFactory(
-        c -> new LogInController(new AppManager(new InMemoryGolfAppModelDao())));
-    final Parent root = loader.load();
-    controller = loader.getController();
-    final var scene = new Scene(root);
-    stage.setScene(scene);
-    stage.show();
+    loadFxml(stage);
   }
 
-  @Test
-  void testController() {
-    assertNotNull(controller);
+  @Override
+  String fxmlName() {
+    return "LogIn.fxml";
+  }
+
+  @Override
+  LogInController controllerFactory() {
+    return new LogInController(new AppManager(new InMemoryGolfAppModelDao()));
   }
 
   @Test
