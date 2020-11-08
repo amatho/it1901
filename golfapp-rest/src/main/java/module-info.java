bf7bb9e1cc62474ae8546b446d5056e38e21fd9a
Amandus Søve Thorsrud <ama.thorsrud@gmail.com>
2020-11-08 16:37:00 +0100

    Add module-info.java to golfapp-rest

---
+++ golfapp-rest/src/main/java/module-info.java
@@ -0,0 +1,12 @@
+open module golfapp.rest {
+  requires golfapp.core;
+  requires hk2.api;
+  requires jersey.media.json.jackson;
+  requires jersey.server;
+  requires java.ws.rs;
+  requires com.fasterxml.jackson.databind;
+  requires javax.inject;
+
+  exports golfapp.rest.server;
+  exports golfapp.rest.api;
+}
