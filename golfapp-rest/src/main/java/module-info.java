e1f2fb993afd03e246eb3bc580f5cc887ed06658
Amandus Søve Thorsrud <ama.thorsrud@gmail.com>
2020-11-08 16:43:33 +0100

    Fix missing requires in golfapp-rest module-info

--- golfapp-rest/src/main/java/module-info.java
+++ golfapp-rest/src/main/java/module-info.java
@@ -2,6 +2,7 @@ open module golfapp.rest {
   requires golfapp.core;
   requires hk2.api;
   requires jersey.media.json.jackson;
+  requires jersey.common;
   requires jersey.server;
   requires java.ws.rs;
   requires com.fasterxml.jackson.databind;
